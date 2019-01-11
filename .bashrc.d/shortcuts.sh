f3e496d0d6388367de952f17f254093d2bfb6c13
Joe Wilner <jhwilner1@gmail.com>
2019-01-11 11:32:33 -0500

    Fleshes it out

--- .bashrc.d/shortcuts.sh
+++ .bashrc.d/shortcuts.sh
@@ -1,8 +1,15 @@
+# because docker-compose is a long f'ing name
 function dcmp {
   docker-compose "${@}"
 }
 
+# file list -- primes fzf with my defaults when you're looking at files
+function fl {
+  fzf --preview='bat --color=always {}'
+}
+
+# jump to editing a file from a list
 function jmp {
-  local found=$(fzf --preview='bat --color=always {}') || return 1
+  local found=$(fl) || return 1
   "${EDITOR:-vi}" "${found}"
 }
