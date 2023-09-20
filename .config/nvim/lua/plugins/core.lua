4070d5cb28e55b343b60db75562dcfe95e996835
Joe Wilner <jhwilner1@gmail.com>
2023-09-20 19:40:55 -0400

    Add trouble
    Add trouble for better error message viewing and set rust editorconfig

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -64,4 +64,13 @@ return {
     end,
   },
   "direnv/direnv.vim",
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
