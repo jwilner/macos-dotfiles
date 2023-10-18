bda33b39c405f80dd4b1743ac8af808e10002f01
Joe Wilner <jhwilner1@gmail.com>
2023-10-18 12:48:14 -0400

    Interactive search and replace wrapper
    Script a simple interactive search and replace wrapper.
    
    Also disable some package module within starship.

--- .config/shortcuts.sh
+++ .config/shortcuts.sh
@@ -13,3 +13,15 @@ function jmp {
   found=$(fl "${@}") || return 1
   "${EDITOR:-vi}" "${found}"
 }
+
+sandr() {
+  local search="${1}" replace="${2}"
+  if [[ -z "${search}" ]] || [[ -z "${replace}" ]]; then
+    echo "Usage: sandr SEARCH REPLACE" >&2
+    return 1
+  fi
+
+  while read -r fname; do
+    vim -c '%s!'"${search}"'!'"${replace}"'!gc' -c 'wq' "${fname}"
+  done < <(rg -l "${search}")
+}
